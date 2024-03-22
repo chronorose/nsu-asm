f81fa01ce5e1581eb1ce6cdcc9fec87e69fb4a20
chronorose <rendemare@gmail.com>
2024-03-22 07:22:13 +0300

    macro change

--- task4.asm
+++ task4.asm
@@ -1,54 +1,5 @@
-.text
-
-.macro syscall %n
-	li a7, %n
-	ecall
-.end_macro
-
-.macro readch
-	syscall 12
-.end_macro
-
-.macro push %register
-	addi sp, sp, -4
-	sw %register, 0(sp)
-.end_macro
-
-.macro pop %register
-	lw %register, 0(sp)
-	addi sp, sp, 4
-.end_macro
-
-.macro error %s
-.data
-str: .asciz %s
-.text
-	newline
-	la a0, str
-	syscall 4
-	exit 1
-.end_macro
-
-.macro newline
-	push a0
-	li a0, 10
-	printch
-	pop a0
-.end_macro
-
-.macro println
-	newline
-	printch
-.end_macro
-
-.macro exit %n
-	li a0, %n
-	syscall 93
-.end_macro
-
-.macro printch
-	syscall 11
-.end_macro
+j main
+.include "macro.asm"
 
 main:
 	call read_num_prologue
--- task5.asm
+++ task5.asm
@@ -1,6 +1,6 @@
 j main
 .include "macro.asm"
-.include "hex_out.asm"
+.include "hex_io.asm"
 .text
 main:
 	call readnum
@@ -14,8 +14,7 @@ main:
 	exit 0
 
 multiplication:
-	push s0
-	li s0, 0
+	li t4, 0
 	li t0, 32
 	li t2, 0x1
 mul_main:
@@ -24,12 +23,11 @@ mul_main:
 	and t3, t2, a1
 	beqz t3, mul_main_cont
 	sll t3, a0, t1
-	add s0, s0, t3
+	add t4, t4, t3
 mul_main_cont:
 	slli t2, t2, 1
 	addi t0, t0, -1
 	bnez t0, mul_main
 mul_epilogue:
-	mv a0, s0
-	pop s0
+	mv a0, t4
 	ret
